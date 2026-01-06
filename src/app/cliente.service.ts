9d600caeb5412d611962358b91b79cfc41a8a410
brenoaissa <brenoaugusto05@hotmail.com.br>
2026-01-06 17:37:53 -0300

    feat: create method to get user from localStorage

--- src/app/cliente.service.ts
+++ src/app/cliente.service.ts
@@ -6,9 +6,24 @@ import { Cliente } from './cadastro/cliente';
 })
 export class ClienteService {
 
+  static REPO_CLIENTES = "_CLIENTES";
+
   constructor() { }
 
   salvar(cliente: Cliente){
     
+  }
+
+  obterStorage() : Cliente[]{
+    const repositorioClientes = localStorage.getItem(ClienteService.REPO_CLIENTES);
+    
+    if(repositorioClientes){
+      const clientes: Cliente[] = JSON.parse(repositorioClientes);
+      return clientes;
+    }
+
+    const clientes: Cliente[] = [];
+    localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(clientes));
+    return clientes;
   }
 }
